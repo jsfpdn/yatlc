44b8c5b53f5674a7467ff45320c8a6acacba9411
Josef Podaný <jsfpdn@gmail.com>
2023-03-12 21:29:15 +0100

    add: tokenizer struct, cli args

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,54 @@
 const std = @import("std");
+const clap = @import("clap");
+
+const fs = std.fs;
+const io = std.io;
+
+const tokenizer = @import("tokenizer/tokenizer.zig");
+
+// TODO(jsfpdn):
+//  * pass files from stdin
+//  * parse command line arguments
+
+const MAX_BYTES: usize = 1024 * 1024;
+
+const params = clap.parseParamsComptime(
+    \\-h, --help
+    \\-v, --verbose
+    \\<str>
+    \\
+);
 
 pub fn main() !void {
-    std.debug.print("This is the yatl compiler.", .{});
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{ .diagnostic = &diag }) catch |err| {
+        diag.report(io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+    if (res.args.help)
+        std.debug.print("help!");
+    if (res.args.verbose)
+        std.debug.print("verbose!");
+    for (res.positionals) |pos|
+        std.debug.print("positional: {s}", .{pos});
+
+    const file: fs.File = try fs.cwd().openFile("build.zig", .{});
+    defer file.close();
+
+    const reader = file.reader();
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    const contents = try reader.readAllAlloc(allocator, MAX_BYTES);
+    defer allocator.free(contents);
+
+    const tok = tokenizer.Tokenizer{ .contents = contents };
+    tok.printContents();
 }
 
 test {
     // Driver code to run all tests in descendant sub-packages.
-    const tokenizer = @import("tokenizer/tokenizer.zig");
-
     std.testing.refAllDeclsRecursive(tokenizer);
     std.testing.refAllDeclsRecursive(@This());
 }
--- src/tokenizer/tokenizer.zig
+++ src/tokenizer/tokenizer.zig
@@ -1,6 +1,14 @@
 const tokens = @import("tokens.zig");
 const std = @import("std");
 
+pub const Tokenizer = struct {
+    contents: []const u8,
+
+    pub fn printContents(self: Tokenizer) void {
+        std.debug.print("{s}\n", .{self.contents});
+    }
+};
+
 test {
     // Driver code to run all tests in this package.
     std.testing.refAllDeclsRecursive(tokens);
