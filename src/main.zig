dc490792187720a411f3c67eef16d7283daabb78
Josef Podaný <jsfpdn@gmail.com>
2023-03-17 07:44:34 +0100

    improve: cli ux

--- src/main.zig
+++ src/main.zig
@@ -9,29 +9,49 @@ const logger = @import("logger/logger.zig");
 
 const MAX_BYTES: usize = 1024 * 1024;
 
+const description = "yatlc is a yet-another-toy-language compiler.";
 const params = clap.parseParamsComptime(
-    \\-h, --help
-    \\-v, --verbose
-    \\<str>
+    \\-h, --help        Show this help message
+    \\-v, --verbose     Print debug information
+    \\<str>             Path to a source file to be compiled
     \\
 );
 
+fn showHelp(writer: anytype) !void {
+    _ = try writer.write(description ++ std.cstr.line_sep ++ "yatlc:" ++ std.cstr.line_sep);
+    try clap.help(writer, clap.Help, &params, .{});
+    _ = try writer.write(std.cstr.line_sep);
+}
+
+fn showUsage(writer: anytype) !void {
+    try clap.usage(writer, clap.Help, &params);
+    _ = try writer.write(std.cstr.line_sep);
+}
+
 pub fn main() !void {
     var diag = clap.Diagnostic{};
+    const errWriter = io.getStdErr().writer();
+
     var res = clap.parse(clap.Help, &params, clap.parsers.default, .{ .diagnostic = &diag }) catch |err| {
-        diag.report(io.getStdErr().writer(), err) catch {};
-        return err;
+        diag.report(errWriter, err) catch {};
+        try showUsage(errWriter);
+        return;
     };
     defer res.deinit();
+
+    if (res.args.help) {
+        try showHelp(errWriter);
+        return;
+    }
 
     const log: logger.Logger = logger.Logger{ .verbose = res.args.verbose };
 
     if (res.positionals.len != 1) {
         log.err("Path to exactly one source file must be supplied!\n", .{});
-        try clap.usage(std.io.getStdErr().writer(), clap.Help, &params);
-        _ = try std.io.getStdErr().writer().write("\n");
+        try showUsage(errWriter);
         return;
     }
+
     const filePath = res.positionals[0];
 
     const file: fs.File = fs.cwd().openFile(filePath, .{}) catch |err| {
